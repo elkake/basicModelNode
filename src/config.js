91f9c346f8203ac9a618a79f4031feb73b1038bd
elkake <gianco.marquez@gmail.com>
2024-12-03 15:36:55 -0500

    base basico de proyecto

--- src/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-export const NAME_BUCKET = process.env.NAME_BUCKET
-export const SERVER_BUCKET = process.env.SERVER_BUCKET
-export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY
-export const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY
